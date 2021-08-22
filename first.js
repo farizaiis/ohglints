d4e60ce723e9a8dad36afaca54e88688661b1670
farizaiis <farizaiis@hotmail.com>
2021-08-22 20:45:48 +0700

    test masalah mas rayan

---
+++ first.js
@@ -0,0 +1,12 @@
+//test trouble mas rayan
+
+const kelas = 10
+console.log(kelas)
+
+const array = ["Rayan", 10, {nama : "Rayan", kelas : 10}]
+console.log(array[2])
+
+const objek = {}
+
+
+// salah file name error
