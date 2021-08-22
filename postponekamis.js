a04a3ec3cc6d7f7537a223782beb02a5ba03050f
farizaiis <farizaiis@hotmail.com>
2021-08-22 14:45:17 +0700

    tugas tentang fungsi date dan switch

---
+++ postponekamis.js
@@ -0,0 +1,16 @@
+const tanggal = new Date("August 17, 1945 07:00:00")
+
+// test bulan dengan integer
+// let stringTanggal = "Sekarang adalah hari jumat, jam " + tanggal.getHours() + ", tanggal " + tanggal.getDate() + " bulan " + tanggal.getMonth() + " tahun " + tanggal.getFullYear()
+
+// test bulan dengan string
+let stringTanggal = "Sekarang adalah hari Jumat, jam " + tanggal.getHours() + ", tanggal " + tanggal.getDate() + " bulan Agustus tahun " + tanggal.getFullYear()
+
+switch (tanggal.getHours()) {
+    case 6:
+        console.log("Beda Jam dengan jawaban")
+        break
+    case 7:
+        console.log(stringTanggal)
+        break
+}
