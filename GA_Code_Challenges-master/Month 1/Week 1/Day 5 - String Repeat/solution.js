b3143d663a04f22d38452ba1c86fdcdb809d71a0
farizaiis <farizaiis@hotmail.com>
2021-08-25 20:40:55 +0700

    tambah jawaban mas jihan dengna for loop

--- GA_Code_Challenges-master/Month 1/Week 1/Day 5 - String Repeat/solution.js
+++ GA_Code_Challenges-master/Month 1/Week 1/Day 5 - String Repeat/solution.js
@@ -1,3 +1,11 @@
 function repeatStr (n, s) {
     return s.repeat(n);
   }
+
+//   function repeatStr (n, s) {
+//     let checkRepeat = "";
+//     for(let i = 0; i < n; i++) {
+//         checkRepeat += s;    
+//     }
+//     return checkRepeat;
+//   }
