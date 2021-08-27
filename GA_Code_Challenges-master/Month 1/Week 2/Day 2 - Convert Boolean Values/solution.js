0ab2a1aa35c07d5042d176b4aeb016461493578a
farizaiis <farizaiis@hotmail.com>
2021-08-27 20:47:20 +0700

    check true boolean

---
+++ GA_Code_Challenges-master/Month 1/Week 2/Day 2 - Convert Boolean Values/solution.js
@@ -0,0 +1,12 @@
+function boolToWord( bool ){
+    //...
+    if (bool === true) {
+      return bool = "Yes"
+    } else {
+      return bool = "No"
+    }
+  }
+
+  function boolToWord( bool ){
+    return bool ? "Yes" : "No" ;
+  }
