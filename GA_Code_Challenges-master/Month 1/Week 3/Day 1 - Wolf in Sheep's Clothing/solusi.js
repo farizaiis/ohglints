fa62f0f6e5061f958348145ac98725af3b956aa7
farizaiis <farizaiis@hotmail.com>
2021-09-03 19:31:54 +0700

    Wolf in sheep cloting code wars

--- GA_Code_Challenges-master/Month 1/Week 3/Day 1 - Wolf in Sheep's Clothing/solusi.js
+++ GA_Code_Challenges-master/Month 1/Week 3/Day 1 - Wolf in Sheep's Clothing/solusi.js
@@ -1,7 +1,7 @@
 function warnTheSheep(queue) {
-    let listSheep = queue.reverse(); // untuk membalikan index, awal ke akhir & akhir ke index
+    let listArr = queue.reverse(); // untuk tuker index, awal ke akhir & akhir ke awal
     for(let i = 0; i < queue.length; i++) {
-        if(listSheep[0] === "wolf")
+        if(listArr[0] === "wolf")
         return "Pls go away and stop eating my sheep";
         else
         return "Oi! Sheep number " + listSheep.indexOf("wolf") + "! You are about to be eaten by a wolf!";
