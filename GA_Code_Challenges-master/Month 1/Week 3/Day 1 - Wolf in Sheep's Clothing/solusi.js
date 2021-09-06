8e3dab8927e3b7a395641464bcc86485d001f57b
farizaiis <farizaiis@hotmail.com>
2021-09-07 05:53:55 +0700

    tambah edited solusi

--- GA_Code_Challenges-master/Month 1/Week 3/Day 1 - Wolf in Sheep's Clothing/solusi.js
+++ GA_Code_Challenges-master/Month 1/Week 3/Day 1 - Wolf in Sheep's Clothing/solusi.js
@@ -7,3 +7,13 @@ function warnTheSheep(queue) {
         return "Oi! Sheep number " + listSheep.indexOf("wolf") + "! You are about to be eaten by a wolf!";
     }
 }
+
+function warnTheSheep(queue) {
+    let listArr = queue.reverse(); // untuk tuker index, awal ke akhir & akhir ke awal
+    for(let i = 0; i < queue.length; i++) {
+        if(listArr[0] === "wolf")
+        return "Pls go away and stop eating my sheep";
+        else
+        return `Oi! Sheep number ${listArr.indexOf("wolf")}! You are about to be eaten by a wolf!`;
+    }
+}
