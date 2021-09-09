564df0013fc40e60d38252c898d056f1466fd762
farizaiis <farizaiis@hotmail.com>
2021-09-09 13:08:23 +0700

    add pharse int

--- GA_Code_Challenges-master/Month 1/Week 3/Day 3 - Parse Nice Int/solusi.js
+++ GA_Code_Challenges-master/Month 1/Week 3/Day 3 - Parse Nice Int/solusi.js
@@ -1,4 +1,28 @@
 function getAge(inputString){
-    // return correct age (int). Happy coding :) 
-      return Number(inputString[0]);
+    switch(inputString){
+      case "1 years old":
+        return 1;
+      case "2 years old":
+        return 2;
+      case "3 years old":
+        return 3;
+      case "4 years old":
+        return 4;
+      case "5 years old":
+        return 5;
+      case "6 years old":
+        return 6;
+      case "7 years old":
+        return 7;
+      case "8 years old":
+        return 8;
+      case "9 years old":
+        return 9;
+      default:
+        return 1;
     }
+  }
+
+  function getAge(inputString){
+    return parseInt(inputString);
+  }
