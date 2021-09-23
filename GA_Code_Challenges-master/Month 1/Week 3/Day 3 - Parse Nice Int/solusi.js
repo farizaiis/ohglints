55d8d575ba5eccb8daa0fed0751293a6b46899d6
farizaiis <farizaiis@hotmail.com>
2021-09-24 00:14:56 +0700

    + Materi Express, include middleware

--- GA_Code_Challenges-master/Month 1/Week 3/Day 3 - Parse Nice Int/solusi.js
+++ GA_Code_Challenges-master/Month 1/Week 3/Day 3 - Parse Nice Int/solusi.js
@@ -24,5 +24,19 @@ function getAge(inputString){
   }
 
   function getAge(inputString){
-    return parseInt(inputString);
+    return parseInt(inputString); //parseint convert string jadi integer
   }
+
+  function getAge(inputString){
+    // return correct age (int). Happy coding :) 
+      let numbers = ["0","1","2","3","4","5","6","7","8","9"]
+      
+      for (let i = 0; i < inputString.length;i++) {
+        if (numbers.includes(inputString[i])) {
+          return Number(inputString[i])
+        }
+      }
+      
+      return "No Number in Sentence"
+    }
+    
